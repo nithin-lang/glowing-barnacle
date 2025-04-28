57ea07a7b2c70e67b407adb5fc2d52281e3ec7a1
Nithin <nithin_v@hotmail.com>
2025-04-28 12:44:51 +0530

    Variables

---
+++ compound-data-types/src/main.rs
@@ -0,0 +1,15 @@
+// Compound Data Types
+// arrays, tuples, slices and strings (slice string)
+
+
+fn main() {
+
+
+    //Array
+    let numbers: [i32; 5] = [1,2,3,4,5];
+    println!("Array numbers: {:?}", numbers);
+
+    // Error
+    // let mix: [i32; 5] = [1,2,"Apple",false,5];
+    // println!("Mix: {:?}", mix);
+}
---
+++ variables/src/main.rs
@@ -0,0 +1,30 @@
+// Signed Integers: i8, i16, i32, i64, i128
+// Unsigned Integers: u8, u16, u32, u64, u128
+
+fn main() {
+    let x: i32 = -172;
+    let y: u64 = 52;
+
+    println!("Signed Integer: {}", x);
+    println!("Unsigned Integer: {}", y);
+
+// Floats
+// f32, f64
+
+    let pi: f64 = 3.14;
+    println!("Pi value: {}", pi);
+
+// Boolean
+// true/false
+
+    let is_alive: bool = false;
+    println!("Is alive? {}", is_alive);
+
+// Character
+// char
+
+    let letter: char = 'z';
+    println!("Letter: {}", letter);
+
+}
+
